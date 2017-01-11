370254f882bf9b9978acb038c6421aec2642034b
dimazotoff8@gmail.com <rubikoid@macbook-air-rubikoid.local>
2017-01-11 21:17:50 +0300

    Initial Commit

---
+++ SecureVK/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SecureVK
+//
+//  Created by Rubikoid on 11.01.17.
+//  Copyright © 2017 Rubikoid. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ SecureVK/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SecureVK
+//
+//  Created by Rubikoid on 11.01.17.
+//  Copyright © 2017 Rubikoid. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
